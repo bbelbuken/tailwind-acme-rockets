2d9ae307608464a34d6442734667a670cd4bb1ad
Batuhan Belbüken <bbelbuken@hotmail.com>
2024-10-25 20:17:14 +0300

    final adjustments

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/**/*.{html,js}"], 
+  content: ["./build/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         papayawhip: {
-          light: '#fef4e4',
-          DEFAULT: '#ffefd5',
-          dark: 'fee5bc'
-        }
+          light: "#fef4e4",
+          DEFAULT: "#ffefd5",
+          dark: "fee5bc",
+        },
       },
       screens: {
         widescreen: { raw: "(min-aspect-ratio: 3/2)" },
