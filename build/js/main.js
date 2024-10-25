08bfd526bab3b141c957308141ee02364f8e3242
Batuhan Belbüken <bbelbuken@hotmail.com>
2024-10-25 18:59:26 +0300

    added script to html

---
+++ build/js/main.js
@@ -0,0 +1,15 @@
+const initApp = () => {
+    const hamburgerBtn = document.getElementById("hamburger-button")
+    const mobileMenu = document.getElementById("mobile-menu")
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle("hidden")
+        mobileMenu.classList.toggle("flex")
+    }
+
+    hamburgerBtn.addEventListener("click", toggleMenu)
+    mobileMenu.addEventListener("click", toggleMenu)
+
+}
+document.addEventListener('DOMContentLoaded', initApp) 
+// we don't directly call the function bc the buttons won't be able to selected yet.
